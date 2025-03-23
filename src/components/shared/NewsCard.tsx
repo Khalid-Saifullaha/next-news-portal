b7feead8d2e32ef43cd9cbf16782533d0f730299
Your Name <khalidsaifullaha3@gmail.com>
2025-03-23 16:31:36 +0600

    car card added

--- src/components/shared/NewsCard.tsx
+++ src/components/shared/NewsCard.tsx
@@ -3,29 +3,53 @@ import Link from "next/link";
 import React from "react";
 
 import newsImage from "@/assests/banner.jpg";
+import carImage from "@/assests/patrik-storm-alstra-pictures-eDyallVm_8A-unsplash.jpg";
 import { Button } from "../ui/button";
 
 const NewsCard = () => {
   return (
-    <div className="border p-4 rounded-md shadow-md">
-      <Link href="/">
-        <Image
-          src={newsImage}
-          alt="newsImage"
-          className="mb-b md:h-56 rounded hover:scale-105 cursor-pointer transition-all duration-200"
-        />
-      </Link>
-      <div>
-        <h2 className="text-xl font-semibold my-3">
-          Progressively recaptiualize goal-oriented action items via.
-        </h2>
-        <p className="mb-4">
-          Synergistically brand granular e-tailers for prospective metrics.
-          Professionally formulate wireless methods of.
-        </p>
+    <div className="">
+      <div className="border p-4 rounded-md shadow-md">
         <Link href="/">
-          <Button variant="default">Read More</Button>
+          <Image
+            src={newsImage}
+            alt="newsImage"
+            className="mb-b md:h-56 rounded hover:scale-105 cursor-pointer transition-all duration-200"
+          />
         </Link>
+        <div>
+          <h2 className="text-xl font-semibold my-3">
+            Progressively recaptiualize goal-oriented action items via.
+          </h2>
+          <p className="mb-4">
+            Synergistically brand granular e-tailers for prospective metrics.
+            Professionally formulate wireless methods of.
+          </p>
+          <Link href="/">
+            <Button variant="default">Read More</Button>
+          </Link>
+        </div>
+      </div>
+      <div className="border p-4 rounded-md shadow-md">
+        <Link href="/">
+          <Image
+            src={carImage}
+            alt="newsImage"
+            className="mb-b md:h-56 rounded hover:scale-105 cursor-pointer transition-all duration-200"
+          />
+        </Link>
+        <div>
+          <h2 className="text-xl font-semibold my-3">
+            Progressively recaptiualize goal-oriented action items via.
+          </h2>
+          <p className="mb-4">
+            Synergistically brand granular e-tailers for prospective metrics.
+            Professionally formulate wireless methods of.
+          </p>
+          <Link href="/">
+            <Button variant="default">Read More</Button>
+          </Link>
+        </div>
       </div>
     </div>
   );
