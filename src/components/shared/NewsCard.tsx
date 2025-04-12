0c13bd87d8812106f77ca9433b12a0bc885f88c7
Your Name <khalidsaifullaha3@gmail.com>
2025-04-12 09:27:56 +0600

    car blog remove

--- src/components/shared/NewsCard.tsx
+++ src/components/shared/NewsCard.tsx
@@ -31,27 +31,6 @@ const NewsCard = () => {
           </Link>
         </div>
       </div>
-      <div className="border p-4 rounded-md shadow-md">
-        <Link href="/">
-          <Image
-            src={carImage}
-            alt="newsImage"
-            className="mb-b md:h-56 rounded hover:scale-105 cursor-pointer transition-all duration-200"
-          />
-        </Link>
-        <div>
-          <h2 className="text-xl font-semibold my-3">
-            Progressively recaptiualize goal-oriented action items via.
-          </h2>
-          <p className="mb-4">
-            Synergistically brand granular e-tailers for prospective metrics.
-            Professionally formulate wireless methods of.
-          </p>
-          <Link href="/">
-            <Button variant="default">Read More</Button>
-          </Link>
-        </div>
-      </div>
     </div>
   );
 };
