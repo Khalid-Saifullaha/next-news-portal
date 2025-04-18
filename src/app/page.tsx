b6141b2d8dfae1ca258f4ae527c8c7f0a4859827
Your Name <khalidsaifullaha3@gmail.com>
2025-04-18 16:47:23 +0600

    extra featurs remove

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,6 @@ export default function Home() {
     <div className="py-12">
       <Banner />
 
-      {/* Featured Section */}
       <div className="my-16 px-4 md:px-8">
         <h2 className="text-3xl font-bold text-center mb-10 text-purple-700">
           🌟 Featured
