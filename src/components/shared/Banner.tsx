6d9d04ca49991d26ccdc5fe101dea0377ec8542a
Your Name <khalidsaifullaha3@gmail.com>
2025-04-19 08:15:15 +0600

    banner colore changed

--- src/components/shared/Banner.tsx
+++ src/components/shared/Banner.tsx
@@ -5,18 +5,18 @@ import bannerImg from "@/assests/banner.jpg";
 
 const Banner = () => {
   return (
-    <div className="bg-slate-200">
+    <div className="bg-gradient-to-r from-purple-100 to-indigo-100">
       <div className="px-4 py-8 lg:px-8 grid grid-cols-1 md:grid-cols-2 items-center gap-8">
         {/* image */}
         <div>
           <Image src={bannerImg} alt="bannerImg" />
         </div>
         <div className="space-y-4 flex flex-col">
-          <h4 className="text-sm font-medium text-gray-500">Technology</h4>
-          <h2 className="text-3xl font-bold">
+          <h4 className="text-sm font-medium text-purple-800">Technology</h4>
+          <h2 className="text-3xl font-bold text-gray-900">
             Synergistically underwhelm intermandated users rather.
           </h2>
-          <p>
+          <p className="text-gray-800">
             Authoritatively redefine pandemic opportunities without front-end
             systems. Synergistically visualize inexpensive innovation after
             global leadership skills. Globally harness superior imperatives
@@ -27,7 +27,12 @@ const Banner = () => {
             ROI after reliable e-business. Globally predominate technically
             sound models.
           </p>
-          <Button variant="default">Read More</Button>
+          <Button
+            variant="default"
+            className="bg-purple-600 hover:bg-purple-700 text-white"
+          >
+            Read More
+          </Button>
         </div>
       </div>
     </div>
